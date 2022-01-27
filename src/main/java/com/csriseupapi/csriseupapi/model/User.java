dc488730c11067217f051b79185073e7f09cb2af
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-27 17:09:17 -0500

    init user model

--- src/main/java/com/csriseupapi/csriseupapi/model/User.java
+++ src/main/java/com/csriseupapi/csriseupapi/model/User.java
@@ -1,2 +1,11 @@
-package com.csriseupapi.csriseupapi.model;public class User {
+package com.csriseupapi.csriseupapi.model;
+
+import java.util.List;
+
+public class User {
+    private Long id;
+    private String userName;
+    private String emailAddress;
+    private String password;
+    private List<Job> jobList;
 }
