3b7a4e7f70d8f4a8dcc0c664f881387ee2a4dd23
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-27 10:48:05 -0500

    created project

---
+++ src/test/java/com/csriseupapi/csriseupapi/CsriseupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csriseupapi.csriseupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsriseupApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
