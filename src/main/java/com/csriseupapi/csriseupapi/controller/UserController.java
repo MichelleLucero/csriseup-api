b260480eacb1637fdca039ed7b993569ed881699
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-28 18:56:28 -0500

    can now register a user

--- src/main/java/com/csriseupapi/csriseupapi/controller/UserController.java
+++ src/main/java/com/csriseupapi/csriseupapi/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.csriseupapi.csriseupapi.controller;
-
-import com.csriseupapi.csriseupapi.repository.UserRepository;
+import com.csriseupapi.csriseupapi.model.User;
 import com.csriseupapi.csriseupapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 
+@RestController
+@RequestMapping(path = "/auth/users")
 public class UserController {
     private UserService userService;
     private static final Logger LOGGER = Logger.getLogger(UserController.class.getName());
@@ -15,5 +20,9 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @PostMapping("/register")
+    public User createUser(@RequestBody User userObject){
+        LOGGER.info("calling createUser method from controller");
+        return userService.createUser(userObject);
+    }
 }
--- src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
+++ src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
@@ -4,27 +4,35 @@ import com.csriseupapi.csriseupapi.exception.InformationExistException;
 import com.csriseupapi.csriseupapi.model.User;
 import com.csriseupapi.csriseupapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
 public class UserService {
+
     private UserRepository userRepository;
-    private static final Logger LOGGER = Logger.getLogger(UserService.class.getName());
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User userObject){
-        LOGGER.info("calling createUser method from service");
-        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+    public User createUser(User userObject) {
+        System.out.println("service calling createUser ==>");
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
             return userRepository.save(userObject);
         } else {
-            throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " is already exists");
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
+                    " already exists");
         }
-
+    }
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
     }
 }
