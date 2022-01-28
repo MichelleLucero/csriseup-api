8e164cd9790806394814c44697c2929a43223c3c
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-28 16:20:32 -0500

    added user and user profile

--- src/main/java/com/csriseupapi/csriseupapi/repository/UserRepository.java
+++ src/main/java/com/csriseupapi/csriseupapi/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.csriseupapi.csriseupapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//    to register
+    boolean existsByEmailAddress(String userEmailAddress);
+//    to login
+    User findUserByEmailAddress(String userEmailAddress);
 }
--- src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
+++ src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
@@ -1,5 +1,7 @@
 package com.csriseupapi.csriseupapi.service;
 
+import com.csriseupapi.csriseupapi.exception.InformationExistException;
+import com.csriseupapi.csriseupapi.model.User;
 import com.csriseupapi.csriseupapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,15 @@ public class UserService {
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(User userObject){
+        LOGGER.info("calling createUser method from service");
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " is already exists");
+        }
+
     }
 }
