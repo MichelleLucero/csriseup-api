d2072a0985db9ddac672ade399994d5124254577
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-27 22:41:48 -0500

    added repositories

---
+++ src/main/java/com/csriseupapi/csriseupapi/repository/PositionRepository.java
@@ -0,0 +1,7 @@
+package com.csriseupapi.csriseupapi.repository;
+
+import com.csriseupapi.csriseupapi.model.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PositionRepository extends JpaRepository<Position, Long> {
+}
