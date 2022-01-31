6adea48cc5762e6d44a7ad806396f2a1e88d238e
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2022-01-31 17:25:11 -0500

    updated to return token for both register and login

--- src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
+++ src/main/java/com/csriseupapi/csriseupapi/service/UserService.java
@@ -39,11 +39,17 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User userObject) {
+    public ResponseEntity<?> createUser(User userObject) {
         System.out.println("service calling createUser ==>");
         if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
             userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
-            return userRepository.save(userObject);
+//          return token for new user
+            userRepository.save(userObject);
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(userObject.getEmailAddress());
+            final String token = jwtUtils.generateToken(userDetails);
+            return ResponseEntity.ok(new LoginResponse(token));
+//            return token for new user end
+//            return userRepository.save(userObject);
         } else {
             throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
                     " already exists");
@@ -55,8 +61,8 @@ public class UserService {
                 new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
         );
         final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
-        final String jwt = jwtUtils.generateToken(userDetails);
-        return ResponseEntity.ok(new LoginResponse(jwt));
+        final String token = jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new LoginResponse(token));
     }
 
     public User findUserByEmailAddress(String email) {
